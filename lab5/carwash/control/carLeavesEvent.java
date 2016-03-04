9903054f8a28d438aacf9de630db09708f549e13
linarv-4 <linus_95_arvidsson@hotmail.com>
2016-03-04 13:44:49 +0100

    Update carLeavesEvent.java

--- lab5/carwash/control/carLeavesEvent.java
+++ lab5/carwash/control/carLeavesEvent.java
@@ -1,2 +1,46 @@
 package lab5.carwash.control;
 
+public class carLeavesEvent {
+	private Car myCar;
+	private carwashState myCarwashState;
+	private boolean slowCheck;
+	private boolean fastCheck;
+
+	public carLeavesEvent(carwashState setState) {
+		this.myCarwashState = setState;
+	}
+
+	public void execute() {
+		this.myCar = createCar();
+	}
+
+	private Car createCar() {
+		return this.myCarwashState.carFactory.createCar();
+	}
+
+	private void checkWashers() {
+		if (myCarwashState.numFreeSlow < 2) {
+			slowCheck = true;
+		} else {
+			slowCheck = false;
+		}
+		if (myCarwashState.numFreeFast < 2) {
+			fastCheck = true;
+		} else {
+			fastCheck = false;
+		}
+
+	}
+
+	private void washCar() {
+
+	}
+
+	private void createEventForWash() {
+
+	}
+
+	private void queueCar(){
+		
+	}
+}
