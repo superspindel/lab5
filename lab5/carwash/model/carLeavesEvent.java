c589332d3ebd29ccafec10f57b52a7d5536ab621
superspindel <viktor.fallman@hotmail.com>
2016-03-07 20:00:23 +0100

    Update carLeavesEvent.java

--- lab5/carwash/model/carLeavesEvent.java
+++ lab5/carwash/model/carLeavesEvent.java
@@ -1,62 +1,58 @@
-package lab5.carwash.control;
+package lab5.carwash.model;
+import lab5.general.control.Event;
+import lab5.general.control.EventQueue;
 
-public class carLeavesEvent {
-	private Car myCar;
+public class carLeavesEvent extends Event{
 	private carwashState myCarwashState;
-	private boolean slowCheck;
-	private boolean fastCheck;
-
-	public carLeavesEvent(carwashState setState) {
+	private EventQueue myQueue;
+	private double TimeForEvent;
+	private Car thisCar;
+	private int washerUsed;
+	
+	public carLeavesEvent(carwashState setState, EventQueue myQueue, double timeForEvent, Car leavingCar, int washerUsed)
+	{
 		this.myCarwashState = setState;
-	}
-
-	public void execute() {
-		this.myCar = createCar();
-	}
-
-	private Car createCar() {
-		return this.myCarwashState.carFactory.createCar();
-	}
-
-	private void checkWashers() {
-		if (myCarwashState.numFreeSlow < 2) {
-			slowCheck = true;
-		} else {
-			slowCheck = false;
-		}
-		if (myCarwashState.numFreeFast < 2) {
-			fastCheck = true;
-		} else {
-			fastCheck = false;
-		}
-
-	}
-
-	private void washCar() {
-		checkWashers();
-		if(slowCheck == true){
-			myCarwashState.numFreeSlow--;
-			//Ändra state
-			return;
-		} 
-		if((fastCheck == true)){
-			myCarwashState.numFreeFast--;
-			//Ändra state
-			return;
-		}
-	}
-
-	private void createEventForWash() {
-
-	}
-
-	private void queueCar(){
-		checkWashers();
-		if(slowCheck == false && fastCheck == false){
-			FIFO.add(myCar);
-		}
-	}
-	private void updateStates(){
+		this.myQueue = myQueue;
+		this.TimeForEvent = timeForEvent;
+		this.thisCar = leavingCar;
+		this.washerUsed = washerUsed;
 		
+	}
+	public void execute()
+	{
+		updateStats();
+		if(!(this.myCarwashState.isQueueEmpty()))
+		{
+			if(this.washerUsed ==  1)
+			{
+				createFastWashEvent(this.myCarwashState.getCarFromQueue());
+			}
+			else if(this.washerUsed == 2)
+			{
+				createSlowWashEvent(this.myCarwashState.getCarFromQueue());
+			}
+		}
+	}
+	private void createFastWashEvent(Car myCar)
+	{
+		carLeavesEvent thisCarLeaves = new carLeavesEvent(this.myCarwashState, this.myQueue, myCar, this.TimeForEvent+this.myCarwashState.fastWashTime);
+		this.myQueue.add(thisCarLeaves);
+	}
+	private void createSlowWashEvent(Car myCar)
+	{
+		carLeavesEvent thisCarLeaves = new carLeavesEvent(this.myCarwashState, this.myQueue, myCar, this.TimeForEvent+this.myCarwashState.slowWashTime);
+		this.myQueue.add(thisCarLeaves);
+	}
+	
+	/**
+	 * update 	free washers
+	 * 			idleMachineTime
+	 * 			rejectedCars
+	 * 			lastEventTime
+	 */
+	private void updateStats()
+	{
+		this.myCarwashState.idleMachineTime += (this.TimeForEvent-this.myCarwashState.lastEvent)*(this.myCarwashState.numFreeFast+this.myCarwashState.numFreeSlow);
+		this.myCarwashState.queueTime += (this.TimeForEvent-this.myCarwashState.lastEvent)*(this.myCarwashState.sizeOfQueue());
 	}
 }
