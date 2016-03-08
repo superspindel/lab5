d3eed6609c20f32a087a3b13a599d330808ee1f5
linarv-4 <linus_95_arvidsson@hotmail.com>
2016-03-08 15:34:53 +0100

    Update Simulator.java

--- lab5/general/control/Simulator.java
+++ lab5/general/control/Simulator.java
@@ -1,2 +1,12 @@
 package lab5.general.control;
 
+public class Simulator {
+		boolean flag = true;
+		public Simulator(){
+			while(flag == true || EventQueue.isEmpty() == false){
+				EventQueue.getFirst().execute();
+			}
+		}
+}
+
+
