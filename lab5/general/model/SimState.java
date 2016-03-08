15f8d818805d4c367f770370398d273264e50e9a
linarv-4 <linus_95_arvidsson@hotmail.com>
2016-03-08 16:56:29 +0100

    Update SimState.java

--- lab5/general/model/SimState.java
+++ lab5/general/model/SimState.java
@@ -2,6 +2,12 @@ package lab5.general.model;
 
 import java.util.Observable;
 
-public class SimState extends Observable{
+public class extends Observable{
+
+	private boolean active;
+	private float time;
 	
+	boolean isActive(){
+		return active;
+	}
 }
