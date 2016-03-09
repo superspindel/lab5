59cd67f4e09c90b71516cb8eecb1afb07e0bfef8
linarv-4 <linus_95_arvidsson@hotmail.com>
2016-03-09 10:44:20 +0100

    Update carwashView.java

--- lab5/carwash/view/carwashView.java
+++ lab5/carwash/view/carwashView.java
@@ -11,3 +11,16 @@ public void getMessage(){
 public void setMessage(String newMessage){
   message = newMessage;
 }
+	public void firstPrint(){
+		System.out.println("Fast machines: "+myState.numfreefast;)
+		System.out.println("Slow machines: "+myState.numfreeslow;)
+		System.out.println("Fast distribution: "+myState.fastDis)
+		System.out.println("Slow distribution: "+myState.slowDis)
+		System.out.println("Exponential distribution with lambda = "+myState.lambda);
+		System.out.println("Seed: "+myState.seed);
+		System.out.println("Max Queue size: "+myState.maxQueueSize);
+		System.out.println("----------------------------------------");
+	}
+	public void afterPrint(){
+	  
+	}
