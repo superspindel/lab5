e3b0fbd6ed750029c4a62a366ebc2f3bd7e0f8c8
linarv-4 <linus_95_arvidsson@hotmail.com>
2016-03-09 09:47:02 +0100

    Update carwashView.java

--- lab5/carwash/view/carwashView.java
+++ lab5/carwash/view/carwashView.java
@@ -1,2 +1,13 @@
 package lab5.carwash.view;
 
+private String message;
+
+public carwashView(){
+  
+}
+public void getMessage(){
+  return message;
+}
+public void setMessage(String newMessage){
+  message = newMessage;
+}
